56cfae5f4bdac3270bf3bd6a0ac9490b40552631
caiosntos <caiogiovanists@outlook.com>
2025-06-16 19:30:00 -0300

    Inicializando Exercicio 1 da OT4 de PHP

---
+++ arrayLivros.php
@@ -0,0 +1,11 @@
+<?php
+
+$livro = array(
+    "Titulo" => "Nada pode me parar",
+    "Autor" => "David Goggins",
+    "Ano" => "2018"
+);
+
+$livro["Informações"] = "Editora Sextante";
+
+print_r($livro);
