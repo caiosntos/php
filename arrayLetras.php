8f2b8f8ef2ec52d33fcf6e6d0aad46e55942364e
caiosntos <caiogiovanists@outlook.com>
2025-06-16 19:43:25 -0300

    Finalizando Exercicio 4 da OT4 de php

---
+++ arrayLetras.php
@@ -0,0 +1,11 @@
+<?php
+
+$afalbeto = array("a",  "b", "c", "d", "e", "f", "g", "h", "i", "j", "a", "a", "a", "a", "a", "a");
+
+$contagem = array_count_values($afalbeto);
+
+print_r($contagem);
+
+
+
+
